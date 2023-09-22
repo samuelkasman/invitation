2af52d506ad8bdc9bdd1ec78b5e79c3def6374fd
Samuel Kasman <samuel.kasman@creativedock.com>
2023-09-22 23:34:12 +0200

    invitation page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,61 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className={styles.main}>
-      {/* <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-          </a>
+    <div className={styles.wrapper}>
+      <div className={styles.bgTop} />
+
+      <main className={styles.main}>
+        <div className={styles.logo}>
+          <Image src={"/logo.png"} alt="Monika" width={200} height={200} />
         </div>
-      </div> */}
 
-      <div className={styles.center}>Monikina oslava</div>
+        <div className={styles.description}>
+          Drahí blízki, pozývam Vás na rodinno-priateľské posedenie pri
+          príležitosti oslavy mojich narodenín.
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Kedy <span>-&gt;</span>
-          </h2>
-          <p>Oslava sa bude konat XX.10.2023</p>
-        </a>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <h2>Kedy?</h2>
+            <p>Oslava sa bude konať 21.10.2023 o 15:00</p>
+          </div>
 
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Kde <span>-&gt;</span>
-          </h2>
-          <p>Bude to na tomto mieste</p>
-        </a>
+          <div className={styles.card}>
+            <h2>Kde?</h2>
+            <a
+              href="https://www.google.com/maps/place/SP+Resort+%26+Spa/@49.2266,18.7050993,17z/data=!3m1!4b1!4m9!3m8!1s0x47145f724dc50191:0x2b58e785fd7dc297!5m2!4m1!1i2!8m2!3d49.2266!4d18.7076742!16s%2Fg%2F11stnn4d27?entry=ttu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              SP Resort
+            </a>
 
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Co doniest <span>-&gt;</span>
-          </h2>
-          <p>Seba a dobru naladu</p>
-        </a>
+            <p>Pietna 11, 010 04 Žilina, Slovakia</p>
+          </div>
 
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Program <span>-&gt;</span>
-          </h2>
-          <p>Liner, posedenie, a tak a to</p>
-        </a>
-      </div>
-    </main>
+          <div className={styles.card}>
+            <h2>Čo si obliecť?</h2>
+            <p>Čokoľvek v čom Vám je pohodlne, nemusíte mat kravaty a opatky</p>
+          </div>
+
+          <div className={styles.card}>
+            <h2>Čo Vás čaká?</h2>
+            <p>Neskorý obed,</p>
+            <p>torta a káva,</p>
+            <p>večerný bufet</p>
+            <p>a ja!</p>
+          </div>
+        </div>
+
+        <div className={styles.footer1}>Teším sa na Vás,</div>
+        <div className={styles.footer2}>Monika.</div>
+      </main>
+
+      <div className={styles.bgBottom} />
+    </div>
   );
-}
+};
+
+export default Home;
